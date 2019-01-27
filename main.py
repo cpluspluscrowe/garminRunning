73e5c135bcd0346c1e3eafffb7e51db7f0d85cc3
Chad Crowe <ccrowe@linkedin.com>
2019-01-27 12:54:54 -0600

    I have created a df from the garmin file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fitparse import FitFile
 import os
 import pandas
+from collections import defaultdict
+from copy import deepcopy
+
 data = "./data"
 for file in os.listdir(data):
     if ".fit" in file:
@@ -8,8 +11,7 @@ for file in os.listdir(data):
         fit_file = FitFile(path)
 
 
-def get_distinct_keys(fit_file):
-    messages = list(fit_file.get_messages())
+def get_distinct_keys(messages):
     message_dict = list(map(lambda x: x.get_values(), messages))
     items = list(map(lambda x: x.items(), message_dict))
     
@@ -20,11 +22,38 @@ def get_distinct_keys(fit_file):
     filter_out_unknown = list(filter(lambda x: not "unknown_" in x, keys_distinct))
     return filter_out_unknown
 
+def create_row_template(columns):
+    d = {}#defaultdict(lambda: None)
+    for column in columns:
+        d[column] = None
+    return d
+
+def fill_row(template, message):
+    """
+    Mutates row to contain garmin data for the given columns
+    """
+    row = deepcopy(template)
+    as_d = message.get_values()
+    for key,value in as_d.items():
+        if key in row:
+            row[key] = value
+    return row
+
+def create_df_from_rows(rows):  
+    df = pandas.DataFrame([], columns=row_template.keys())
+    for row in rows:
+        new_df = pandas.DataFrame([list(row.values())], columns = list(row.keys()))
+        df = df.append(new_df)
+    return df
+
+messages = list(fit_file.get_messages())
+keys = get_distinct_keys(messages)
+row_template = create_row_template(keys)
+rows = list(map(lambda message: fill_row(row_template, message), messages))
+df = create_df_from_rows(rows)
 
 
-keys = get_distinct_keys(fit_file)
 
-print(keys)
 
 #df = pandas.DataFrame(messages)
 #df.to_csv("./{0}.csv".format(file), sep=',',index=False)
