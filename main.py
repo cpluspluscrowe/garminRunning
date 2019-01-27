fc6b6eab72ab6ceda107099a16ead4c1fb9e68a4
Chad Crowe <ccrowe@linkedin.com>
2019-01-27 10:16:51 -0600

    Getting distinct columns from fit files in order to create a dataframe with info on the relevant columns

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 27 08:49:44 2019
+from fitparse import FitFile
+import os
+import pandas
+data = "./data"
+for file in os.listdir(data):
+    if ".fit" in file:
+        path = os.path.join(data,file)
+        fit_file = FitFile(path)
 
-@author: ccrowe
-"""
+def get_distinct_keys(fit_file):
+    messages = list(fit_file.get_messages())
+    message_dict = list(map(lambda x: x.get_values(), messages))
+    items = list(map(lambda x: x.items(), message_dict))
+    
+    filter_none_columns =  list(map(lambda item:list(filter(lambda x: x[1] != None, item)), items))
+    
+    keys_all = list(map(lambda dict_entry: list(map(lambda x: x[0], dict_entry)), filter_none_columns))
+    keys_distinct = set([item for sublist in keys for item in sublist])
+    return keys_distinct
 
+
+
+keys = get_distinct_keys(fit_file)
+
+
+
+#df = pandas.DataFrame(messages)
+#df.to_csv("./{0}.csv".format(file), sep=',',index=False)
