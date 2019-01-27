691c4c12155eb64b9f95466632760653e2b6014e
Chad Crowe <ccrowe@linkedin.com>
2019-01-27 10:21:41 -0600

    updated readme and filtered out unknown columns

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ for file in os.listdir(data):
         path = os.path.join(data,file)
         fit_file = FitFile(path)
 
+
 def get_distinct_keys(fit_file):
     messages = list(fit_file.get_messages())
     message_dict = list(map(lambda x: x.get_values(), messages))
@@ -15,14 +16,15 @@ def get_distinct_keys(fit_file):
     filter_none_columns =  list(map(lambda item:list(filter(lambda x: x[1] != None, item)), items))
     
     keys_all = list(map(lambda dict_entry: list(map(lambda x: x[0], dict_entry)), filter_none_columns))
-    keys_distinct = set([item for sublist in keys for item in sublist])
-    return keys_distinct
+    keys_distinct = set([item for sublist in keys_all for item in sublist])
+    filter_out_unknown = list(filter(lambda x: not "unknown_" in x, keys_distinct))
+    return filter_out_unknown
 
 
 
 keys = get_distinct_keys(fit_file)
 
-
+print(keys)
 
 #df = pandas.DataFrame(messages)
 #df.to_csv("./{0}.csv".format(file), sep=',',index=False)
